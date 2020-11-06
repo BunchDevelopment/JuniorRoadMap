f8fc322df477675b85d9042b5d7bbddc7264e63c
Brandon Bunch <brandonbunch@Brandons-MBP.attlocal.net>
2020-11-05 19:28:02 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+    </div>
+  );
+}
+
+export default App;
